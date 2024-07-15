6b898481f13ca0fc252f040ed181b10e59afe213
Vinita Tandulkar <vinitatandulkar29@gmail.com>
2024-07-15 18:10:50 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="css/workflow.css"/>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+function showImg()
+{
+var obj=document.getElementById('Analysis');
+obj.className = 'show';
+}
+</script>
+</head>
+<body>
+
+		<div class="svg-wrap">
+			<svg width="64" height="64" viewBox="0 0 64 64">
+				<path id="arrow-left-1" d="M46.077 55.738c0.858 0.867 0.858 2.266 0 3.133s-2.243 0.867-3.101 0l-25.056-25.302c-0.858-0.867-0.858-2.269 0-3.133l25.056-25.306c0.858-0.867 2.243-0.867 3.101 0s0.858 2.266 0 3.133l-22.848 23.738 22.848 23.738z" />
+			</svg>
+			<svg width="64" height="64" viewBox="0 0 64 64">
+				<path id="arrow-right-1" d="M17.919 55.738c-0.858 0.867-0.858 2.266 0 3.133s2.243 0.867 3.101 0l25.056-25.302c0.858-0.867 0.858-2.269 0-3.133l-25.056-25.306c-0.858-0.867-2.243-0.867-3.101 0s-0.858 2.266 0 3.133l22.848 23.738-22.848 23.738z" />
+			</svg>
+			<svg width="64" height="64" viewBox="0 0 64 64">
+				<path id="arrow-left-2" d="M26.667 10.667q1.104 0 1.885 0.781t0.781 1.885q0 1.125-0.792 1.896l-14.104 14.104h41.563q1.104 0 1.885 0.781t0.781 1.885-0.781 1.885-1.885 0.781h-41.563l14.104 14.104q0.792 0.771 0.792 1.896 0 1.104-0.781 1.885t-1.885 0.781q-1.125 0-1.896-0.771l-18.667-18.667q-0.771-0.813-0.771-1.896t0.771-1.896l18.667-18.667q0.792-0.771 1.896-0.771z" />
+			</svg>
+			<svg width="64" height="64" viewBox="0 0 64 64">
+				<path id="arrow-right-2" d="M37.333 10.667q1.125 0 1.896 0.771l18.667 18.667q0.771 0.771 0.771 1.896t-0.771 1.896l-18.667 18.667q-0.771 0.771-1.896 0.771-1.146 0-1.906-0.76t-0.76-1.906q0-1.125 0.771-1.896l14.125-14.104h-41.563q-1.104 0-1.885-0.781t-0.781-1.885 0.781-1.885 1.885-0.781h41.563l-14.125-14.104q-0.771-0.771-0.771-1.896 0-1.146 0.76-1.906t1.906-0.76z" />
+			</svg>
+			<svg width="64" height="64" viewBox="0 0 64 64">
+				<path id="arrow-left-3" d="M44.797 17.28l0.003 29.44-25.6-14.72z" />
+			</svg>
+			<svg width="64" height="64" viewBox="0 0 64 64">
+				<path id="arrow-right-3" d="M19.203 17.28l-0.003 29.44 25.6-14.72z" />
+			</svg>
+			<svg width="64" height="64" viewBox="0 0 64 64">
+				<path id="arrow-left-4" d="M15.946 48l0.003-10.33 47.411 0.003v-11.37h-47.414l0.003-10.304-15.309 16z" />
+			</svg>
+			<svg width="64" height="64" viewBox="0 0 64 64">
+				<path id="arrow-right-4" d="M48.058 48l-0.003-10.33-47.414 0.003v-11.37h47.418l-0.003-10.304 15.306 16z" />
+			</svg>
+			<svg width="64" height="64" viewBox="0 0 64 64">
+				<path id="arrow-left-5" d="M48 10.667q1.104 0 1.885 0.781t0.781 1.885-0.792 1.896l-16.771 16.771 16.771 16.771q0.792 0.792 0.792 1.896t-0.781 1.885-1.885 0.781q-1.125 0-1.896-0.771l-18.667-18.667q-0.771-0.771-0.771-1.896t0.771-1.896l18.667-18.667q0.771-0.771 1.896-0.771zM32 10.667q1.104 0 1.885 0.781t0.781 1.885-0.792 1.896l-16.771 16.771 16.771 16.771q0.792 0.792 0.792 1.896t-0.781 1.885-1.885 0.781q-1.125 0-1.896-0.771l-18.667-18.667q-0.771-0.771-0.771-1.896t0.771-1.896l18.667-18.667q0.771-0.771 1.896-0.771z" />
+			</svg>
+			<svg width="64" height="64" viewBox="0 0 64 64">
+				<path id="arrow-right-5" d="M29.333 10.667q1.104 0 1.875 0.771l18.667 18.667q0.792 0.792 0.792 1.896t-0.792 1.896l-18.667 18.667q-0.771 0.771-1.875 0.771t-1.885-0.781-0.781-1.885q0-1.125 0.771-1.896l16.771-16.771-16.771-16.771q-0.771-0.771-0.771-1.896 0-1.146 0.76-1.906t1.906-0.76zM13.333 10.667q1.104 0 1.875 0.771l18.667 18.667q0.792 0.792 0.792 1.896t-0.792 1.896l-18.667 18.667q-0.771 0.771-1.875 0.771t-1.885-0.781-0.781-1.885q0-1.125 0.771-1.896l16.771-16.771-16.771-16.771q-0.771-0.771-0.771-1.896 0-1.146 0.76-1.906t1.906-0.76z" />
+			</svg>
+		</div>
+		<div class="container">
+			<!-- Top Navigation -->
+            <div class="row">
+            <div class="col-md-2">
+            <img src="img/img/circle.jpg" alt="001" width="380px" height="auto" style="padding-top:60px;"/>
+            </div>
+            <div class="col-md-5" style="padding-top:30px">
+			<div style="">
+			<section class="color-1">
+				<h2  style="margin-top: 6px; margin-bottom: 2px;">Slide</h2>
+				<nav class="nav-slide">
+					<a class="prev" href="/item1">
+						<span class="icon-wrap" style="background-color:gold;"><svg class="icon" width="32" height="32" viewBox="0 0 64 64"><use xlink:href="#arrow-left-1"></svg></span>
+						<div>
+							<input type="button" onclick = "showImg()" value= "Analysis" style="padding:20px;background-color:transparent;border-color:transparent;"/>
+						</div>
+					</a>
+					
+				</nav>
+			</section>
+            </div>
+            
+            <div style="margin-left:80px;margin-bottom:40px;">
+            <section class="color-1">
+				<h2 style="margin-top: 6px; margin-bottom: 2px;">Slide</h2>
+				<nav class="nav-slide">
+					<a class="prev" href="/item1">
+						<span class="icon-wrap" style="padding:10px;margin-bottom:8px;background-color:#F90;"><svg class="icon" width="32" height="32" viewBox="0 0 64 64"><use xlink:href="#arrow-left-1"></svg></span>
+						<div>
+							<input type="button" value="Planning" id="theButton" style="padding:20px;background-color:transparent;border-color:transparent;"/>
+						</div>
+					</a>
+					
+				</nav>
+			</section>
+            </div>
+            
+            <div style="margin-left:100px;margin-bottom:40px;">
+            <section class="color-1">
+				<h2 style="margin-top: 6px; margin-bottom: 2px;">Slide</h2>
+				<nav class="nav-slide">
+					<a class="prev" href="/item1">
+						<span class="icon-wrap" style="padding:10px;margin-bottom:8px;background-color:#F30;"><svg class="icon" width="32" height="32" viewBox="0 0 64 64"><use xlink:href="#arrow-left-1"></svg></span>
+						<div>
+							<input type="button" value="Acceptance" id="theButton1" style="padding:20px;background-color:transparent;border-color:transparent;"/>
+						</div>
+					</a>
+					
+				</nav>
+			</section>
+            </div>
+            
+            
+			<div style="margin-left:80px;margin-bottom:40px;">
+            <section class="color-1">
+				<h2 style="margin-top: 6px; margin-bottom: 2px;">Slide</h2>
+				<nav class="nav-slide">
+					<a class="prev" href="/item1">
+						<span class="icon-wrap" style="padding:10px;margin-bottom:8px;background-color:#60C;"><svg class="icon" width="32" height="32" viewBox="0 0 64 64"><use xlink:href="#arrow-left-1"></svg></span>
+						<div>
+							<input type="button" value="Development" id="theButton2" style="padding:20px;background-color:transparent;border-color:transparent;"/>
+						</div>
+					</a>
+					
+				</nav>
+			</section>
+            </div>
+            
+            <div style="margin-left:20px;margin-bottom:10px;">
+            <section class="color-1">
+				<h2 style="margin-top: 6px; margin-bottom: 2px;">Slide</h2>
+				<nav class="nav-slide">
+					<a class="prev" href="/item1" >
+						<span class="icon-wrap" style="padding:10px;margin-bottom:8px;background-color:#09F;"><svg class="icon " width="32" height="32" viewBox="0 0 64 64"><use xlink:href="#arrow-left-1"></svg></span>
+						<div>
+							<input type="button" value="Deployment" id="theButton3" style="padding:20px;background-color:transparent;border-color:transparent;"/>
+						</div>
+					</a>
+				</nav>
+			</section>
+            </div>
+            </div>
+        <div class="col-md-5">
+       		<div class="videoframe">
+            <img id="Analysis" src="img/img/1.gif" class="hide">
+            </div>
+        </div>	
+		</div>	
+		</div><!-- /container -->
+        
+        
+		<script>
+			// For Demo purposes only
+			[].slice.call( document.querySelectorAll('nav > a') ).forEach( function(el) {
+				el.addEventListener( 'click', function(ev) { ev.preventDefault(); } );
+			} );
+			 theButton.onclick = function pictureChange()
+			{
+			document.getElementById('Analysis').src="img/img/4.gif";
+			}
+			
+			theButton1.onclick = function pictureChange()
+			{
+			document.getElementById('Analysis').src="img/img/5.gif";
+			}
+			
+			theButton2.onclick = function pictureChange()
+			{
+			document.getElementById('Analysis').src="img/img/6.gif";
+			}
+			theButton3.onclick = function pictureChange()
+			{
+			document.getElementById('Analysis').src="img/img/3.gif";
+			}
+	  </script>
+      </body>
+</html>
